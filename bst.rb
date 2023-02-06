e1a07b322524140183a7df757fd06e00f182e090
ayedun <88404572+ayedun@users.noreply.github.com>
2023-02-06 12:36:09 -0800

    Add insert

--- bst.rb
+++ bst.rb
@@ -4,7 +4,7 @@ class Bst
     def initialize(array)
         @array = clean_array(array)
         @root = build_tree
-        p @array
+       
     end
 
     def clean_array(array)
@@ -26,6 +26,73 @@ class Bst
         puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
         pretty_print(node.is_left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.is_left
     end
+
+    def insert(value, current_node = @root)
+        if current_node.nil?
+            p current_node
+            return false
+        elsif value > current_node.data
+            if !(insert(value, current_node.is_right))
+                current_node.is_right = Node.new(value)
+                p current_node.data 
+            end
+        elsif value < current_node.data
+            if !(insert(value, current_node.is_left))
+                current_node.is_left = Node.new(value)
+                p current_node.data
+            end
+        end
+        return true      #Reset the boolean or else it will not work
+        
+
+        
+    end
+
+    def delete(value, current_node = @root, state = false)
+        if value == current_node.data
+            state = true
+            p "found ya"
+            if !(current_node.is_right.nil?) && !(current_node.is_left.nil?)
+                p " both kids"
+                current_node.data = find_next_biggest_node(current_node)
+                
+
+                return 
+            elsif current_node.is_right.nil? && current_node.is_left.nil?
+                p"both no kids"
+                return
+            else 
+                current_node.is_right.nil? ? (p "right no kids") : (p "left no kids")
+                return
+            end
+
+            return
+
+
+
+
+
+
+
+
+
+
+
+
+
+        elsif value > current_node.data
+            delete(value, current_node.is_right) unless current_node.is_right.nil?
+            p current_node.data
+        elsif value < current_node.data
+            delete(value, current_node.is_left) unless current_node.is_left.nil?
+            p current_node.data
+        end
+
+        if !state
+            return state
+        end
+
+    end
     
 
 
@@ -33,5 +100,13 @@ class Bst
 end
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 tree = Bst.new(array)
+p "Raw tree"
 tree.pretty_print
+tree.insert(6)
+p "after insert"
+
+tree.pretty_print
+if !(tree.delete(7))
+    p "Didn't find ya"
+end
 
